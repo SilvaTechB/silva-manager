a5e621c68ad384d770824a4474890a67bdfeb96d
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2026-03-28 22:19:25 +0000

    Use local silva-library-android-1.3.0.aar (no POM in registry yet)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     // Silva
     implementation(libs.arsclib)
     implementation(libs.silva.patcher)
-    implementation(libs.silva.library.android)
+    implementation(files("libs/silva-library-android-1.3.0.aar"))
 
     // Exclude xmlpull as it's included in Android already
     configurations.configureEach {

86c6fdd751882035443c0deb854115def7a9242a
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2026-03-28 21:50:13 +0000

    Bump version to 1.12.7

--- build-debug.sh
+++ build-debug.sh
@@ -26,7 +26,7 @@ export TMPDIR=$WORK_TMPDIR
 export TMP=$WORK_TMPDIR
 export TEMP=$WORK_TMPDIR
 
-APP_VERSION="1.12.6"
+APP_VERSION="1.12.7"
 
 echo "=== Silva Manager Build v$APP_VERSION ==="
 echo "Android SDK: $ANDROID_HOME"
